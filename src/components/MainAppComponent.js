30f6fd97d018bc14e815130a2874cd0ebccda75e
TJ <tjpataria@gmail.com>
2021-04-27 14:27:54 +0100

    added example component to test packages/code for comment thread

--- src/components/MainAppComponent.js
+++ src/components/MainAppComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header.js';
 import PostComponent from './PostComponent.js';
 import './MainAppComponent.css';
+import CommentExampleActions from './CommentExampleActions.js';
 
 const MainAppComponent = () => {
     
@@ -9,6 +10,7 @@ const MainAppComponent = () => {
         <div className="main">
             <Header />
             <PostComponent />
+            <CommentExampleActions />
         </div>
     )
 }
